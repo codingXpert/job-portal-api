d7d2ecbe46a9f03810ad91423ca23fa3dc80e1ce
Vivek <vivekraj78965.vr@gmail.com>
2023-09-23 16:16:53 +0530

    handling field validation using middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,9 @@
 import User from '../models/user.js';
 
 export const registerController = async (req, res, next) => {
-        const {firstName, lastName, email, password} = req.body;
-    if(!firstName){
-        next('first name is required');
-    }
+    const {firstName, lastName, email, password} = req.body;
 
-    if(!lastName){
-        next('last name is required');
-    }
-
-    if(!email){
-        next('email is required');
-    }
-
-    if(!password){
-        next('password is required and should be greater than six characters');
-    }
-
-    const existingUser = await User.findOne({ email });
-    if(existingUser) {
-        next('Email already registered plz login');
-    }
-
+    // try-catch is automatically handling by the package "express-async-errors"
     const newUser = await User.create({firstName, lastName, email, password});
     return res.status(201).send({
         success: true,
--- middlewares/customError.js
+++ middlewares/customError.js
@@ -1,10 +1,24 @@
-const errorMiddleware = (error, req, res, next) => {
-    console.log(error);
-    res.status(400).send({
-        success: false,
-        message: 'Something Went Wrong',
-        error
-    })
-}
+const errorMiddleware = (err, req, res, next) => {
+  console.log(err);
+  const defaultError = {
+    statusCode: 500,
+    message: err,
+  };
 
+  //missing fields error
+  if (err.name === "ValidationError") {
+    (defaultError.status = 400),
+      (defaultError.message = Object.values(err.errors)
+        .map((item) => item.message)
+        .join(","));
+  }
+
+  // duplicate error
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = 400;
+    defaultError.message = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+
+  res.status(defaultError.statusCode).json({ message: defaultError.message });
+};
 export default errorMiddleware;
