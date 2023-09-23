39821fefa264df1fb67d14340e8375c9996b927e
Vivek <vivekraj78965.vr@gmail.com>
2023-09-23 10:50:36 +0530

    cors and morgan setup

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+//packages import
 import dotenv from 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+//file import
 import db from './config/mongoose.js';
 
 const app = express();
 const port = process.env.port || 5000;
 db();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));  // will log the current route and time taken by it to execute in terminal
 
 app.listen(port, (err) => {
     if(err) {
