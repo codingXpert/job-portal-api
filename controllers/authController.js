7157ee6d0727463720149d4183279652d46a6604
Vivek <vivekraj78965.vr@gmail.com>
2023-09-24 22:31:16 +0530

    jwt token generation from model

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,9 +5,11 @@ export const registerController = async (req, res, next) => {
 
     // try-catch is automatically handling by the package "express-async-errors"
     const newUser = await User.create({firstName, lastName, email, password});
+    const token = newUser.createJWT();
     return res.status(201).send({
         success: true,
         message: 'User Created Successfully',
-        newUser
+        newUser,
+        token
     });  
 }
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from 'bcryptjs'
+import JWT from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema(
   {
@@ -24,7 +25,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: [6, 'password length should be greater than six character']
+      minlength: [6, 'password length should be greater than six character'],
     },
 
     location: {
@@ -40,5 +41,10 @@ userSchema.pre('save', async function () {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt)
 });
+
+// JSON WEBTOKEN
+userSchema.methods.createJWT = function () {
+  return JWT.sign({userId: this._id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+}
 
 export default mongoose.model("User", userSchema);
