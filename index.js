625d653e807fee61a44693b396a13b3527683248
Vivek <vivekraj78965.vr@gmail.com>
2023-10-20 15:32:48 +0530

    security fix using node js external packages

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
+//security packages
+import helmet from 'helmet';
+import xss_clean from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize'
+
 //file import
 import db from './config/mongoose.js';
 import routes from './routes/index.js';
 import customError from './middlewares/customError.js';
 
+//rest object
 const app = express();
+
 const port = process.env.port || 5000;
 db();
 
+//middlewares
+app.use(helmet()); // secure our header section
+app.use(xss_clean()); // checks and sanitize the input coming from POST, GET, params
+app.use(mongoSanitize()); // middleware which sanitizes user-supplied data to prevent MongoDB Operator Injection from(req.body, req.query or req.params).
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));  // will log the current route and time taken by it to execute in terminal
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,16 @@ import { registerController } from '../controllers/authController.js';
 import { loginController } from '../controllers/authController.js';
 import { updateUserController } from '../controllers/authController.js';
 import userAuth from '../middlewares/authMiddleware.js';
+import { rateLimit } from 'express-rate-limit'
 const router = express.Router();
+
+const limiter = rateLimit({ // applies access limit to any route/API
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	// store: ... , // Use an external store for more precise rate limiting
+})
 
 
 router.post('/register', registerController);
